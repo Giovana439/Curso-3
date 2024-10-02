876b934054a29c27313d1649550ec560af7faff2
Giovana439 <116595560+Giovana439@users.noreply.github.com>
2024-10-02 11:11:05 -0300

    s

--- Graficos/quantidadeUsuarios.js
+++ Graficos/quantidadeUsuarios.js
@@ -12,20 +12,22 @@ async function quantidadeUsuariosPorRede() {
             x: nomeDasRedes, 
             y: quantidadeDeUsuarios, 
             type: 'bar',
-            marker:{
+            marker: {
                 color: getCSS('--primary-color')
             }
         }
     ]
 
     const layout = {
-        plot_bgcolor: getCSS(--bg-color)
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+
     }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuariosPorRede()
